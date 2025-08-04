e1d00fbfd64c1c35da006df0a9899a70e8acef61
sorinaji <37400002+sori883@users.noreply.github.com>
2025-08-04 23:44:16 +0900

    init

---
+++ provider.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ap-northeast-1"
+  profile = "${var.profile}"
+}
+
+terraform {
+  required_version = ">= 1.11.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.49.0"
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,2 @@
+variable profile { type = string }
+
